be7113ab795b5826feb96d9aa3c22edf71c8ab56
akshaypandhare <59005128+akshaypandhare@users.noreply.github.com>
2022-01-31 17:57:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,119 +1,54 @@
-variable "ami_type" {
-  description = "(optional)"
-  type        = string
-  default     = null
-}
-
-variable "capacity_type" {
-  description = "(optional)"
-  type        = string
-  default     = null
-}
-
 variable "cluster_name" {
-  description = "(required)"
+  description = "Required"
   type        = string
 }
 
-variable "disk_size" {
-  description = "(optional)"
-  type        = number
-  default     = null
-}
-
-variable "force_update_version" {
-  description = "(optional)"
-  type        = bool
-  default     = null
-}
-
-variable "instance_types" {
-  description = "(optional)"
-  type        = list(string)
-  default     = null
-}
-
-variable "labels" {
-  description = "(optional)"
-  type        = map(string)
-  default     = null
-}
-
-variable "node_group_name" {
-  description = "(required)"
+variable "env_name" {
+  description = "Required"
   type        = string
 }
 
-#variable "node_role_arn" {
-#  description = "(required)"
-#  type        = string
-#}
-
-variable "release_version" {
-  description = "(optional)"
+variable "aws_region" {
+  description = "Required"
   type        = string
-  default     = null
 }
 
 variable "subnet_ids" {
-  description = "(required)"
-  type        = set(string)
+  description = "Required"
+  type        = list(string)
 }
 
-variable "tags" {
-  description = "(optional)"
-  type        = map(string)
-  default     = null
-}
-
-variable "k8s_version" {
-  description = "(optional)"
+variable "node_group_name" {
+  description = "Required"
   type        = string
-  default     = null
 }
 
-variable "launch_template" {
-  description = "nested block: NestingList, min items: 0, max items: 1"
-  type = set(object(
-    {
-      id      = string
-      name    = string
-      version = string
-    }
-  ))
-  default = []
+variable "instance_types" {
+  description = "Optional"
+  type        = list(string)
+  default     = ["t3.meduim"]
 }
 
-variable "remote_access" {
-  description = "nested block: NestingList, min items: 0, max items: 1"
-  type = set(object(
-    {
-      ec2_ssh_key               = string
-      source_security_group_ids = set(string)
-    }
-  ))
-  default = []
+variable "min_size" {
+  description = "Optional"
+  type        = string
+  default     = "1"
 }
 
-variable "scaling_config" {
-  description = "nested block: NestingList, min items: 1, max items: 1"
-  type = set(object(
-    {
-      desired_size = number
-      max_size     = number
-      min_size     = number
-    }
-  ))
+variable "max_size" {
+  description = "Optional"
+  type        = string
+  default     = "3"
 }
 
-variable "timeouts" {
-  description = "nested block: NestingSingle, min items: 0, max items: 0"
-  type = set(object(
-    {
-      create = string
-      delete = string
-      update = string
-    }
-  ))
-  default = []
+variable "desired_size" {
+  description = "Optional"
+  type        = string
+  default     = "2"
+}
+
+variable "kubernetes_version" {
+  description = "Optional"
+  type        = list(string)
+  default     = ["1.21"]
 }
